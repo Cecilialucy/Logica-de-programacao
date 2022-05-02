149c61e1f541a6d8e673eea8c420ad4f7a03ea00
Cecilialucy <101018545+Cecilialucy@users.noreply.github.com>
2022-05-02 18:36:25 -0300

    exercício

---
+++ exercicios/controle/if_2.js
@@ -0,0 +1,10 @@
+const nota = 8;
+const bomComportamento = false;
+
+if (nota >= 7 && bomComportamento)
+    console.log('Parabéns!');
+
+if (nota < 7 || !bomComportamento)
+    console.log('Não foi dessa vez!');
+
+console.log('Fim!');
