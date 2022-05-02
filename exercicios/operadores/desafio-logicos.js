f99aa12ea52a290d87787f20f1cd6c34c39e7ede
Cecilialucy <101018545+Cecilialucy@users.noreply.github.com>
2022-05-01 22:50:07 -0300

    novos exercicios

---
+++ exercicios/operadores/desafio-logicos.js
@@ -0,0 +1,13 @@
+// // Duas expressões resultando em TRUE (Aritméticos/Relacionais/Lógicos)
+let verdadeira1 = 2 + 5 >= 7 && 8 > 2;
+let verdadeira2 = !(6 * 6 < 3);
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+//  Duas expressões que resultem em FALSE (Aritméticos/Relacionais/Lógicos)
+let false1 = 5 * 2 >= 10 != 5 + 5 >= 10
+let false2 = 8 > 2 && 3 + 1 >= 5
+
+console.log(false1);
+console.log(false2);  
