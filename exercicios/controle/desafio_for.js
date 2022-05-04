fde7759ab5b79fd4925a28ff35dff96544bf653c
Cecilialucy <101018545+Cecilialucy@users.noreply.github.com>
2022-05-04 19:31:28 -0300

    desafio

---
+++ exercicios/controle/desafio_for.js
@@ -0,0 +1,20 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+// O desafio é exibir no console a estrutura acima sem utilizar números para controlar o laço for.
+
+//  let linha = '';
+
+//  for(let i = 1; i <= 6; i++) {
+//      linha += '#';
+//     console.log(linha);
+//  }
+
+
+for (let s = '#'; s != '#######'; s += '#') {
+    console.log(s);
+}
