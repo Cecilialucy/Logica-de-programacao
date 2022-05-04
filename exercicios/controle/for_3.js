2028d4f57cce8a13c7cc18ea10c84c2ae27c7e75
Cecilialucy <101018545+Cecilialucy@users.noreply.github.com>
2022-05-03 22:59:12 -0300

    exercício

---
+++ exercicios/controle/for_3.js
@@ -0,0 +1,11 @@
+for (let controle = 7; controle >= 3; controle -= 1) {
+    console.log(controle);
+
+}
+
+//  outra forma de escrever o código é: 
+// for (let i = 7; i >= 3; i --) {
+//     console.log(i);
+// }
+
+console.log('Fim!');
