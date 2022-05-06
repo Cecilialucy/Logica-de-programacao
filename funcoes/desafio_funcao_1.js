8c2fab1a274d87a2f92b07b2d7fe8e170308dc33
Cecilialucy <101018545+Cecilialucy@users.noreply.github.com>
2022-05-06 20:21:02 -0300

    desafio

---
+++ funcoes/desafio_funcao_1.js
@@ -0,0 +1,23 @@
+// função: 2 params -> console.log(+)
+// função: 2 params -> console.log(-)
+
+// ...(3, 4)
+// ...(31, 98)
+
+// ...(11, 7)
+// ...(19, 52)
+
+
+function somar(a, b) {
+    console.log(a + b);
+}
+
+function subtrair(c, d) {
+    console.log(c - d);
+}
+
+somar(3, 4);
+somar(31, 98);
+
+subtrair(11, 7);
+subtrair(19, 52);
