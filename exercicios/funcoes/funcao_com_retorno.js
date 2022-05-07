4b0a01822e5aba8413d71e4cd4c7f32868b06737
Cecilialucy <101018545+Cecilialucy@users.noreply.github.com>
2022-05-07 20:19:22 -0300

    Update funcao_com_retorno.js

--- exercicios/funcoes/funcao_com_retorno.js
+++ exercicios/funcoes/funcao_com_retorno.js
@@ -8,12 +8,14 @@ console.log(valor);
 
 
 function textoOuNumero(retornaTexto) {
-    if(retornaTexto) {
-        return 'Sou um texto';
-    } else {
-        return 123;
-    }
+    return retornaTexto ? 'Sou um texto!' : 123;
+    // if(retornaTexto) {
+    //     return 'Sou um texto';
+    // } else {
+    //     return 123;
+    // }
 }
 
 let texto = textoOuNumero(true);
 console.log(texto);
+console.log(textoOuNumero(false));
