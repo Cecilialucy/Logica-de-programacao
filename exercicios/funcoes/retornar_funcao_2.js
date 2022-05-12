f3a308756b0e82bf80a1ec26cc2613c0e1f0c9c2
Cecilialucy <101018545+Cecilialucy@users.noreply.github.com>
2022-05-12 20:13:04 -0300

    exercício

---
+++ exercicios/funcoes/retornar_funcao_2.js
@@ -0,0 +1,13 @@
+function retornaUmaFuncao() {
+    return function () {
+        return function () {
+            return "Boa Tarde!";
+        }
+
+    }
+}
+
+console.log(retornaUmaFuncao);
+console.log(retornaUmaFuncao());
+console.log(retornaUmaFuncao()());
+console.log(retornaUmaFuncao()()());
